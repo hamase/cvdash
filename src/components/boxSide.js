13334bc5d4d8536a6ce722a8b02e6129eb58a9ad
Hamase Shahabbaspour <hamase86@gmail.com>
2020-08-29 01:19:07 +0430

    change db json

--- src/components/boxSide.js
+++ src/components/boxSide.js
@@ -5,14 +5,31 @@ import { Container, Row, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../assets/scss/boxSide.scss'
 // import logo from '../assets/img/logo.png'
+import axios from 'axios';
+
+import API from '../api';
 
 class BoxSide extends React.Component {
+    state = {
+        persons: []
+    }
+
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+            .then(res => {
+                const persons = res.data;
+                this.setState({ persons });
+            })
+    }
+
     render() {
         return (
-            <Col md={12} className="box_side__inner">
+            <Col md={12} className="box_side__inner" >
                 <div className="header">
                     <FontAwesomeIcon icon={faGlobe} />
-                    <p>Social Media</p>
+                    {
+                        this.state.persons.map(person => <p>{person.name}</p>)
+                    }
                 </div>
             </Col>
         );
